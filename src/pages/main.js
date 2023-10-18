2599a6cf6735fa6a1d91cf6752f4ddffa237ce69
Jake Blackburn <jakeblackburn@Johns-MacBook-Air.local>
2023-10-18 16:25:47 -0400

    Add  global variable

--- src/pages/main.js
+++ src/pages/main.js
@@ -3,4 +3,6 @@ import App from '../App.vue'
 import router from '../router'
 import store from '../store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.config.globalProperties.$hostname = 'http://localhost:3000'
+app.use(store).use(router).mount('#app')
