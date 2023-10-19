51b0a2342871698599fb17107c5298e23de5d18b
Jake Blackburn <jakeblackburn@Johns-MacBook-Air.local>
2023-10-19 18:57:03 -0400

    Move express app into index.js

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,4 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const app = require("./server/app.js");
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
@@ -17,3 +10,6 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+exports.api = onRequest(app);
